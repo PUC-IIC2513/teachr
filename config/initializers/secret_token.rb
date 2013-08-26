1c333c0f0b82878889807f6933fb931adf803805
Raúl Montes <raulmt@gmail.com>
2013-08-26 18:21:59 -0400

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Teachr::Application.config.secret_key_base = '4837a0d1e2dd97898e652c895bafed918f5656e00f86f441e5218929620cbd68d4467a31af05e0cc15fb0e5f4c08bb97a0d213fcee0198788a0afb9bbc1708f8'
