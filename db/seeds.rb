e6b0d9e16be7e630b77b9b2501081d7d813c9f4d
Raúl Montes <raulmt@gmail.com>
2013-08-30 16:42:12 -0400

    sedes con datos iniciales momentáneos de la aplicación

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+u1 = User.create(first_name: "Raúl", last_name: "Doe", email: "raul@example.org", password: "hola.123", role: "teacher")
+u2 = User.create(first_name: "Estéban", last_name: "Roe", email: "esteban@example.org", password: "hola.123", role: "student")
+
+r1 = Resource.create(name: "Ruby slides", description: "Características y principales elementos del lenguaje Ruby", user: u1)
+r2 = Resource.create(name: "HTTP slides", description: "HTTP y su implicancia en las rutas de Rails", user: u1)
+
+
+t1 = Tag.create(name: "rails")
+t2 = Tag.create(name: "ruby")
+t3 = Tag.create(name: "http")
+t4 = Tag.create(name: "clases")
+
+
+ResourcesTag.create(user: u1, tag: t2, resource: r1)
+ResourcesTag.create(user: u1, tag: t4, resource: r1)
+ResourcesTag.create(user: u1, tag: t3, resource: r2)
+ResourcesTag.create(user: u1, tag: t4, resource: r2)
