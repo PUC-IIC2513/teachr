da07fe099785ac20ad9cefc215a3f766b11bba89
Raúl Montes <raulmt@gmail.com>
2013-09-02 23:24:09 -0400

    validaciones de User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_many :announcements, dependent: :destroy
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }, uniqueness: true
+  validates :password, confirmation: true, presence: true, length: { in: 8..50 }
+  validates :password_confirmation, presence: true
+  validates :role, inclusion: { in: %w{student teacher assistant} }
+  
 end
