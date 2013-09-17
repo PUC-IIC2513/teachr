fe4b4e26e852717a1959b8045ec077c1742a6e30
Raúl Montes <raulmt@gmail.com>
2013-09-17 11:16:36 -0300

    simplificación de la migración de contraseñas

--- db/migrate/20130917000129_change_password_to_password_digest_to_user.rb
+++ db/migrate/20130917000129_change_password_to_password_digest_to_user.rb
@@ -6,7 +6,7 @@ class ChangePasswordToPasswordDigestToUser < ActiveRecord::Migration
     # transformamos las passwords a un hash de la forma en que lo hace BCrypt
     require 'bcrypt'
     User.all.each do |u|
-      u.password_digest = BCrypt::Password.create(u.password_digest).to_s
+      u.password = u.password_digest
       u.save
     end
   end
