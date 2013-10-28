ab7e76171fd551eb40c27e0edb72d072e3a5b425
Raúl Montes <raulmt@gmail.com>
2013-10-28 18:53:06 -0300

    Validación JS básica de campos required

--- app/assets/javascripts/sign_ups.js
+++ app/assets/javascripts/sign_ups.js
@@ -1,2 +1,38 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+/**
+Validación "required" a cualquier campo con data-validate=required.
+*/
+$(function() {
+  $("#new_user").on("submit", function(e) {
+    var error = false;
+    var $this = $(this);
+    var $required_fields = $this.find("[data-validate=required]");
+    
+    for(var i = 0, len = $required_fields.length; i < len; i++) {
+      var $field = $($required_fields[i]);
+      var value = $field.val();
+      if (!value || value.trim().length === 0) {
+        error = true;
+        $field.parent().addClass("field_with_errors");
+        $errorMessage = $field.next('span.error-message');
+        if($errorMessage.length === 0) {
+          $errorMessage = $('<span class="error-message"></span>').insertAfter($field);
+        }
+        $errorMessage.html('es requerido');
+      } else {
+        // clear error
+        $field.parent().removeClass('field_with_errors');
+        $field.next('span.error-message').hide();
+      }
+    }
+
+    // TODO: crear una clase que encapsule este comportamiento, o al menos extraerlo a una función    
+    
+    if (error) {
+      e.preventDefault();
+    }
+  });
+});
+
